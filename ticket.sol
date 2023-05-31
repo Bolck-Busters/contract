f3607a40a22f7faa7ed3535e7e058ac5d148e8f0
ywonson <ywson51@gmail.com>
2023-05-31 10:11:45 +0900

    updated whole

--- ticket.sol
+++ ticket.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-
 contract Ticket {
-
     address public owner;
-    uint public deposit;     // show deposit
+    uint256 public deposit;
 
+    event TicketBought(address indexed buyer, uint256 amount);
+    event PrizeClaimed(address indexed gamer, uint256 amount);
 
     constructor() {
         owner = msg.sender;
@@ -17,21 +17,33 @@ contract Ticket {
         _;
     }
 
-    function buyTicket(uint256 ticketFee, uint num) external payable returns (bool){
-        require(msg.value > ticketFee * num, "Insufficient payment");
-        deposit = msg.value;
-        return true;  // If ture --> ticket += num in DB
-    }
+    function buyTicket(uint256 ticketFee, uint256 num) external payable returns (bool) {
+        uint256 totalPayment = ticketFee * num;
+        require(msg.value >= totalPayment, "Insufficient payment");
 
+        deposit += totalPayment;
 
-    function claimPrize(address gamer, uint256 amount) public payable onlyOwner returns (bool) {
-        require(address(this).balance >= amount, "Insufficient funds in the contract");
-        payable(gamer).transfer(amount);
+        emit TicketBought(msg.sender, totalPayment);
+        
         return true;
     }
 
+    function claimPrize(address payable gamer, uint256 amount) public onlyOwner returns (bool) {
+        require(address(this).balance >= amount, "Insufficient funds in the contract");
+        gamer.transfer(amount);
 
-    // deposit 늘리기
-    // deposit 빼기
+        emit PrizeClaimed(gamer, amount);
+        
+        return true;
+    }
 
+    function increaseDeposit() external payable {
+        deposit += msg.value;
+    }
+
+    function withdrawDeposit(uint256 amount) external onlyOwner {
+        require(amount <= deposit, "Amount exceeds available deposit");
+        deposit -= amount;
+        payable(owner).transfer(amount);
+    }
 }
